56cdb93c23c6031e6cb8cc06c282070995a40730
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-03-08 12:47:28 +0100

    Day1 Chapter 2 Task 3

--- 2_ES6+/03_arrowFunctions/Zadania/main.js
+++ 2_ES6+/03_arrowFunctions/Zadania/main.js
@@ -1,1 +1,9 @@
+const getSecondMaxNumber = (tab) => {
+    tab.sort((a,b) => a-b);
+    console.log(tab);
+    return tab[tab.length-2];
+}
 
+const arr1 = [2, 3, 1, 6, 100, 49, 5, 7, 8, 9 ];
+console.log(getSecondMaxNumber(arr1));
+//Wynik w consoli: 49 // Największą liczbą w tablicy jest 100, ale my szukaliśmy drugiej największej więc odpowiedzią musi być 49 w tym przypadku
