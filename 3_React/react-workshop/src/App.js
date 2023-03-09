aa63afca3e0f1997b882999e002009f396392d7d
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-03-09 14:36:48 +0100

    Chpter 4

--- 3_React/react-workshop/src/App.js
+++ 3_React/react-workshop/src/App.js
@@ -5,6 +5,8 @@ import Chapter01Task04 from "./components/Chapter01Task04";
 import Chapter02Task02 from "./components/Chapter02Task02";
 import Chapter02Task04 from "./components/Chapter02Task04";
 import Chapter03Task01 from "./components/Chapter03Task01";
+import Chapter03Task03 from "./components/Chapter03Task03";
+import SearchBox from "./components/Chapter04/SearchBox";
 
 function App() {
   const imageUrl = "https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg";
@@ -16,7 +18,9 @@ function App() {
         {/*<Chapter01Task04 />*/}
         {/*<Chapter02Task02 />*/}
         {/*<Chapter02Task04 />*/}
-        <Chapter03Task01 />
+        {/*<Chapter03Task01 />*/}
+        {/*<Chapter03Task03 />*/}
+        <SearchBox />
       </header>
     </div>
   );
---
+++ 3_React/react-workshop/src/components/Chapter04/SearchBox.js
@@ -0,0 +1,10 @@
+const SearchBox = () => {
+    return (
+        <div>
+            <input type="text" />
+            <button>Search</button>
+        </div>
+    );
+};
+
+export default SearchBox;
