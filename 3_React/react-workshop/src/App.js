e352741dcb5271ef4ec8991b50815e43970642bd
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-03-09 15:09:56 +0100

    Chapter 5

--- 3_React/react-workshop/src/App.js
+++ 3_React/react-workshop/src/App.js
@@ -2,9 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from "./components/Chapter05/Hello";
 import CurrencyConverter from "./components/Chapter04/CurrencyConverter";
+import LikeBox from "./components/Chapter04/LikeBox";
+import Menu from "./components/Chapter04/Menu";
 
 function App() {
   const imageUrl = "https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg";
+  const menu = [
+      {
+          url: "/",
+          text: "Strona główna"
+      },
+      {
+          url: "/blog",
+          text: "Blog"
+      },
+      {
+          url: "/cennik",
+          text: "Cennik"
+      },
+      {
+          url: "/kontakt",
+          text: "Kontakt"
+      }
+  ];
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +36,8 @@ function App() {
             rate={1.125}
         />
         <Hello name="Grzegorz" surname="Kowalski" />
+        <LikeBox likes={550} />
+        <Menu tab={menu} />
       </header>
     </div>
   );
