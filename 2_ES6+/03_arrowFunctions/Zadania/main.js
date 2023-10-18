e994ac6a5f737440a32e5e190906b91dcd3ac1eb
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-10-18 12:31:58 +0200

    Day 1 Chapter 2/3 task 2

--- 2_ES6+/03_arrowFunctions/Zadania/main.js
+++ 2_ES6+/03_arrowFunctions/Zadania/main.js
@@ -1,1 +1,16 @@
+//Zadanie 2
 
+const runInterval = (n = 8) => {
+    let i = 0;
+    const intervalID = setInterval(() => {
+        if (i < n) {
+            console.log("Hello");
+            i++;
+        }
+        else {
+            clearInterval(intervalID);
+        }
+    }, 500);
+}
+
+runInterval();
