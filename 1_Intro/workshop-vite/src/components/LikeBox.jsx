086e7db47d40a99e878ba9dee31518272101e19b
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-10-19 12:37:00 +0200

    component

---
+++ 1_Intro/workshop-vite/src/components/LikeBox.jsx
@@ -0,0 +1,9 @@
+const LikeBox = () => {
+    return (
+        <div style={{width: "100px", height: "50px", borderStyle: "solid"}}>
+            <span>500 likes</span>
+        </div>
+    );
+};
+
+export default LikeBox;
---
+++ 1_Intro/workshop-vite/src/components/Menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <ul>
+            <li><a href="/">Strona główna</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/cennik">Cennik</a></li>
+            <li><a href="/kontakt">Kontakt</a></li>
+        </ul>
+    );
+};
+
+export default Menu;
