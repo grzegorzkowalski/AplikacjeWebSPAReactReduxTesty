b4f0b04c455eaafd45646171ce881e751d8aff93
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-08-28 00:39:49 +0200

    change avatars pics

--- 3_React/03_JSX_zagniezdzenia/people.js
+++ 3_React/03_JSX_zagniezdzenia/people.js
@@ -7,7 +7,7 @@ export default [
     pesel: 66111626126,
     bio:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla, diam non tempus vulputate, tellus massa viverra diam, eget scelerisque ipsum ipsum vel sem.",
-    avatar: "https://fwcdn.pl/fpo/91/13/299113/7332755.3.jpg"
+    avatar: "https://static.vecteezy.com/system/resources/previews/001/993/889/non_2x/beautiful-latin-woman-avatar-character-icon-free-vector.jpg"
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ export default [
     pesel: 53052334461,
     bio:
       "Nullam in dignissim nibh. Sed lorem arcu, consectetur vel posuere quis, cursus sit amet massa. In eleifend ut urna in sodales. Vestibulum viverra lectus in efficitur ultricies.",
-    avatar: "https://fwcdn.pl/fpo/91/13/299113/7332755.3.jpg"
+    avatar: "https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_1280.png"
   },
   {
     id: 3,
@@ -27,6 +27,6 @@ export default [
     pesel: 67032584953,
     bio:
       "Phasellus euismod leo non lacus congue dapibus. Curabitur iaculis faucibus euismod. Vestibulum sem metus, congue at lacinia eget, suscipit at nunc.",
-    avatar: "https://fwcdn.pl/fpo/91/13/299113/7332755.3.jpg"
+    avatar: "https://static.vecteezy.com/system/resources/previews/002/002/257/non_2x/beautiful-woman-avatar-character-icon-free-vector.jpg"
   }
 ];
