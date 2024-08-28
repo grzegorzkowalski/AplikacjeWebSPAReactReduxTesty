21c11855dcdddfae322b2f596a21a2de23618d7a
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-08-28 15:18:49 +0200

    rest, spread 1,2

--- 2_ES6+/04_restSpread/Zadania/main.js
+++ 2_ES6+/04_restSpread/Zadania/main.js
@@ -20,3 +20,33 @@ const stateCopy = {
 }
 
 console.log(stateCopy);
+
+
+//Zadanie 2
+// Stwórz funkcję o nazwie getAverage, która może przyjąć dowolną liczbę parametrów (w postaci liczb) i która zwraca średnią arytmetyczną tych liczb.
+//
+// Przykład wywołania:
+//
+// getAverage(2, 4, 5, 6, 7, 79);
+
+const getAverage = (...params) => {
+    return params.reduce((a,b) => a+b)/params.length
+};
+console.log(getAverage(2, 4, 5, 6, 7, 79));
+console.log(getAverage(2,4,6));
+
+// Zadanie 3
+// Stwórz zmienną przechowującą Twoje imię. Za pomocą operatora rozproszenia zapisz każdą literkę do tablicy. Wypisz tablicę w konsoli.
+//
+const name = "Grzegorz";
+const letters = [...name];
+console.log(letters);
+
+// Zadanie 4
+// Stwórz dwie tablicę fruits i vegetables. Następnie stwórz trzecią tablicę mix, która będzie połączeniem obu poprzednich tablic. Użyj operator rozproszenia.
+
+const vegetables = ["potato", "tomato"];
+const fruits = ["cherry", "peach"];
+
+const salad = [...vegetables, ...fruits];
+console.log(salad);
