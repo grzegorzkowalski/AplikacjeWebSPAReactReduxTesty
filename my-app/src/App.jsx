43df6577932c5d903ea045336ad7617d49df430b
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-08-29 11:37:01 +0200

    Day 2 Chapter 4 Task 1

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,80 +1,82 @@
+import Search from "./components/Search";
 import artist from "./data/artist";
 import fruits from "./data/fruits";
 import people from "./data/people.js";
 import './App.css';
 
 function App() {
-  const numberA = parseInt(prompt("Podaj liczbę A?"));
-  const numberB = parseInt(prompt("Podaj liczbę B?"));
-  const operator = prompt("Wybierz działanie? *,/,-,+")
-  const birthYear = parseInt(prompt("Podaj rok urodzenia?"));
-  const imageUrl = "https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg";
-
-  let result = null;
-  switch (operator) {
-      case "+":
-          result = <h1>{numberA + numberB}</h1>
-          break;
-      case "-":
-          result = <h2>{numberA - numberB}</h2>
-          break;
-      case "*":
-          result = <h3>{numberA * numberB}</h3>
-          break;
-      case "/":
-          if (!numberB) {
-            result = <h4>Błędne dane</h4>
-            break;
-          }
-          result = <h4>{numberA / numberB}</h4>
-          break;
-      default:
-          result = <h5>Błędny wybór</h5>
-          break;
-  }
-
-    const randomNumber = () => {
-        return Math.round(Math.random() * 9 + 1);
-    }
-
-    const randomA = randomNumber();
-    const randomB = randomNumber();
-    const sum = parseInt(prompt(`Podaj wynik dodawania ${randomA} + ${randomB}?`));
+  // const numberA = parseInt(prompt("Podaj liczbę A?"));
+  // const numberB = parseInt(prompt("Podaj liczbę B?"));
+  // const operator = prompt("Wybierz działanie? *,/,-,+")
+  // const birthYear = parseInt(prompt("Podaj rok urodzenia?"));
+  // const imageUrl = "https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg";
+  //
+  // let result = null;
+  // switch (operator) {
+  //     case "+":
+  //         result = <h1>{numberA + numberB}</h1>
+  //         break;
+  //     case "-":
+  //         result = <h2>{numberA - numberB}</h2>
+  //         break;
+  //     case "*":
+  //         result = <h3>{numberA * numberB}</h3>
+  //         break;
+  //     case "/":
+  //         if (!numberB) {
+  //           result = <h4>Błędne dane</h4>
+  //           break;
+  //         }
+  //         result = <h4>{numberA / numberB}</h4>
+  //         break;
+  //     default:
+  //         result = <h5>Błędny wybór</h5>
+  //         break;
+  // }
+  //
+  //   const randomNumber = () => {
+  //       return Math.round(Math.random() * 9 + 1);
+  //   }
+  //
+  //   const randomA = randomNumber();
+  //   const randomB = randomNumber();
+  //   const sum = parseInt(prompt(`Podaj wynik dodawania ${randomA} + ${randomB}?`));
 
   return (
     <>
       <div className="card">
-          <p>{numberA + numberB}</p>
-          <h1>{new Date().getFullYear() - birthYear} lat</h1>
-          {result}
-          <ul>
-              <li>{artist.firstName}</li>
-              <li>{artist.nickname}</li>
-              <li>{artist.lastName}</li>
-              <li>{artist.age}</li>
-          </ul>
-          <p>{fruits.join("-")}</p>
-          <img src={imageUrl} alt="samochód" />
-          <div style={{
-              width: "100px",
-              height: "100px",
-              backgroundColor: sum === randomA + randomB ? "green" : "red"
-          }}>
-              <p>{sum === randomA + randomB ? "Odpowiedź poprawna" : "Odpowiedź błędna"}</p>
-          </div>
-          {
-              people.map((el) => {
-                  return (
-                      <div className="person" key={el.id}>
-                          <img src={el.avatar} alt={el.surname} />
-                          <div className="info">
-                              <h1>{el.title} {el.name} {el.surname}</h1>
-                              <p>{el.bio}</p>
-                          </div>
-                      </div>
-                  )
-              })
-          }
+          {/*<p>{numberA + numberB}</p>*/}
+          {/*<h1>{new Date().getFullYear() - birthYear} lat</h1>*/}
+          {/*{result}*/}
+          {/*<ul>*/}
+          {/*    <li>{artist.firstName}</li>*/}
+          {/*    <li>{artist.nickname}</li>*/}
+          {/*    <li>{artist.lastName}</li>*/}
+          {/*    <li>{artist.age}</li>*/}
+          {/*</ul>*/}
+          {/*<p>{fruits.join("-")}</p>*/}
+          {/*<img src={imageUrl} alt="samochód" />*/}
+          {/*<div style={{*/}
+          {/*    width: "100px",*/}
+          {/*    height: "100px",*/}
+          {/*    backgroundColor: sum === randomA + randomB ? "green" : "red"*/}
+          {/*}}>*/}
+          {/*    <p>{sum === randomA + randomB ? "Odpowiedź poprawna" : "Odpowiedź błędna"}</p>*/}
+          {/*</div>*/}
+          {/*{*/}
+          {/*    people.map((el) => {*/}
+          {/*        return (*/}
+          {/*            <div className="person" key={el.id}>*/}
+          {/*                <img src={el.avatar} alt={el.surname} />*/}
+          {/*                <div className="info">*/}
+          {/*                    <h1>{el.title} {el.name} {el.surname}</h1>*/}
+          {/*                    <p>{el.bio}</p>*/}
+          {/*                </div>*/}
+          {/*            </div>*/}
+          {/*        )*/}
+          {/*    })*/}
+          {/*}*/}
+          <Search />
       </div>
     </>
   )
---
+++ my-app/src/components/Search.jsx
@@ -0,0 +1,10 @@
+const Search = () => {
+    return (
+        <div>
+            <input type="text" />
+            <button>Szukaj</button>
+        </div>
+    );
+};
+
+export default Search;
