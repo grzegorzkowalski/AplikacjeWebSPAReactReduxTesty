a2d9423ee8fd7306cfb6084618d6bd71fe28467f
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-12-03 10:16:17 +0100

    template strings

---
+++ 2_ES6+/06_templateString/Zadania/main.js
@@ -0,0 +1,17 @@
+// Zadanie 2
+
+const person = {
+    name: "Vlad",
+    lastName: "Drăculea",
+    yearOfBirth: 1431,
+    profession: "Lord of Wallachia"
+};
+
+const whoAreYou = ({name, lastName, yearOfBirth, profession}) => {
+    console.log(
+`My name is ${name} ${lastName}.   
+I am ${new Date().getFullYear() - yearOfBirth} years old.
+My profession is ${profession}.`)
+};
+
+console.log(whoAreYou(person));
