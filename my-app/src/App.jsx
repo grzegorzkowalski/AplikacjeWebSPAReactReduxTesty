8332ec0d79da6637226d5c9100ae526e0c229088
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-12-03 12:04:33 +0100

    atrybuty

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,45 +1,22 @@
-import './App.css'
-import person from "./data/artist";
+import './App.css';
 
 function App() {
-  const numberA = parseInt(prompt("Podaj pierwszą liczbę?"));
-  const numberB = parseInt(prompt("Podaj drugą liczbę?"));
-  const birthdate = parseInt(prompt("Podaj rok urodzenia?"));
-  const operator = prompt("Wybierz działanie (+,-,*,/)?");
+  const color = prompt("Wybierz kolor (red, green, blue)?");
+  const isOkColor = ["red", "green", "blue"].includes(color);
 
-  // throw new Error("testujemy");
-  // console.error("Zepsuliśmy");
-  // console.log("test");
-  let result;
-  switch (operator) {
-    case "+":
-      result = <h1>{numberA} + {numberB} = {numberA + numberB}</h1>;
-      break;
-    case "-":
-      result = <h2>{numberA} - {numberB} = {numberA - numberB}</h2>;
-      break;
-    case "*":
-      result = <h3>{numberA} * {numberB} = {numberA * numberB}</h3>;
-      break;
-    case "/":
-      result = <h4>{numberA} / {numberB} = {numberA / numberB}</h4>;
-      break;
-    default:
-      result = <h5>Błedne dane</h5>
-      break;
+  const styles = {
+    width: "100px",
+    height: "100px",
+    borderColor: color,
+    borderStyle: "solid",
+    borderWidth: "5px"
   }
 
   return (
     <>
-      <h2>Suma: {numberA + numberB}</h2>
-      <h3>Twój wiek: {!isNaN(birthdate) ? new Date().getFullYear() - birthdate : "złe dane"}</h3>
-      <ul>
-        <li>{person.firstName}</li>
-        <li>{person.lastName}</li>
-        <li>{person.nickname}</li>
-        <li>{person.age}</li>
-      </ul>
-      {result}
+      {
+        isOkColor ? <div style={styles} /> : <div>Nieprawidłowy wybór koloru</div>
+      }
     </>
   );
 }
---
+++ my-app/src/App2.jsx
@@ -0,0 +1,60 @@
+import './App.css'
+import person from "./data/artist";
+
+function App() {
+  const color = prompt("Wybierz kolor (red, green, blue)?");
+  const isOkColor = ["red", "green", "blue"].includes(color);
+
+  const styles = {
+    width: "100px",
+    height: "100px",
+    borderColor: color,
+    borderStyle: "solid",
+    borderWidth: "5px"
+  }
+  // const numberA = parseInt(prompt("Podaj pierwszą liczbę?"));
+  // const numberB = parseInt(prompt("Podaj drugą liczbę?"));
+  // const birthdate = parseInt(prompt("Podaj rok urodzenia?"));
+  // const operator = prompt("Wybierz działanie (+,-,*,/)?");
+  //
+  // // throw new Error("testujemy");
+  // // console.error("Zepsuliśmy");
+  // // console.log("test");
+  // let result;
+  // switch (operator) {
+  //   case "+":
+  //     result = <h1>{numberA} + {numberB} = {numberA + numberB}</h1>;
+  //     break;
+  //   case "-":
+  //     result = <h2>{numberA} - {numberB} = {numberA - numberB}</h2>;
+  //     break;
+  //   case "*":
+  //     result = <h3>{numberA} * {numberB} = {numberA * numberB}</h3>;
+  //     break;
+  //   case "/":
+  //     result = <h4>{numberA} / {numberB} = {numberA / numberB}</h4>;
+  //     break;
+  //   default:
+  //     result = <h5>Błedne dane</h5>
+  //     break;
+  // }
+
+  return (
+    <>
+      {/*<h2>Suma: {numberA + numberB}</h2>*/}
+      {/*<h3>Twój wiek: {!isNaN(birthdate) ? new Date().getFullYear() - birthdate : "złe dane"}</h3>*/}
+      {/*<ul>*/}
+      {/*  <li>{person.firstName}</li>*/}
+      {/*  <li>{person.lastName}</li>*/}
+      {/*  <li>{person.nickname}</li>*/}
+      {/*  <li>{person.age}</li>*/}
+      {/*</ul>*/}
+      {/*{result}*/}
+      {
+        isOkColor ? <div style={styles} /> : <div>Nieprawidłowy wybór koloru</div>
+      }
+    </>
+  );
+}
+
+export default App;
